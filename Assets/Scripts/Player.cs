ed2a70c33fd6248ef1bc40a29715f4b05548b1ec
Roger Louis <rogerplouis27@gmail.com>
2020-11-05 23:10:25 +1100

    Initial Commit

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,27 +7,23 @@ public class Player : MonoBehaviour
 
     private float moveSpeed = 5f;
     private Vector2 movement;
+    private bool flippedRight = true;
+    private bool flippedLeft = false;
+
+    private bool flip = false; 
 
     public Rigidbody2D rb;
-    public SpriteRenderer sr; 
 
     // Update is called once per frame
     void Update()
     {
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
     {
-        if (movement.x < 0)
-        {
-            sr.flipX = true;
-        } else if (movement.x > 0)
-        {
-            sr.flipX = false;
-        }
-
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
