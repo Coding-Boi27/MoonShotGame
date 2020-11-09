a603b9105fb168d0f93c90b0785b212840fea848
Roger Louis <rogerplouis27@gmail.com>
2020-11-10 07:53:46 +1100

    Added A* Pathfinding

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public Transform player; 
+
+    private Vector3 offset = new Vector3(-2, 0); 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemy();
+    }
+
+    private void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab, transform.position - offset, Quaternion.identity);
+        enemy.GetComponent<EnemyAI>().SetPlayer(player);
+    }
+}
